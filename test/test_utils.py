eb6f5cb8c09cc1fe0efe2291e5625d35653e3199
Debesh-XPS <debesh.mandal@hotmail.com>
2020-05-13 12:37:52 +0100

    updated topology

---
+++ test/test_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+pytest script for testing utils folder
+"""
+from ..utils import *
+from ..utils import simulation
+from ..utils import system
+from ..utils import topology
+
+def test_topology():
+    return True
--- utils/topology.py
+++ utils/topology.py
@@ -1,12 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Objects for handling commonly used high-level readdy topologies
 """
 import readdy
+import numpy as np
 
 class Topology():
     """
     Wrapper for a readdy topology
     """
-    def __init__(self):
+    def __init__(self, top_type : str):
+        self.top_type = top_type
+        self.sequence = []
+        self.positions = np.array([[]])
+
+    def import_dataframe(self, dataframe : pd.DataFrame):
+        """
+        Parse a dataframe containing information on 
+        the sequences and positions of a topology
+        and pass this information to the Topology object
+        """
+        self.sequence = dataframe['sequence']
+        self.positions = dataframe.iloc[:, 1:4].values
+
+    @property
+    def dataframe(self) -> pd.DataFrame:
+        """
+        DataFrame form of sequence and positions
+        """
         pass
+
+    def export_xyz(self, fout : str):
+        pass
+
+    def add_to_sim(self, simulation : readdy.Simulation):
+        """
+        Adds the topology to a readdy simulation
+        """
+        simulation.add_topology(
+            self.top_type,
+            self.sequence,
+            self.positions
+        )
