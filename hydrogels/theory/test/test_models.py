48ad07854cbc8bf153059e45bd4a32a2fe9d62db
Debesh-XPS <debesh.mandal@hotmail.com>
2020-06-10 17:47:23 +0100

    added some testing framework

--- hydrogels/theory/test/test_models.py
+++ hydrogels/theory/test/test_models.py
@@ -1,2 +1,9 @@
-def test_placeholder():
-    return True
+import hydrogels.theory.models as models
+
+def test_utils():
+    return
+
+def test_integrator():
+    import hydrogels.theory.models.integrator as integrator
+    integrator.Simulation(1)
+    return
