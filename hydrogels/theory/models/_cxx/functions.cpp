c07daac842d9eb310fab1fe0f0be469c41f6771e
Debesh-XPS <debesh.mandal@hotmail.com>
2020-06-25 15:55:54 +0100

    added new functions and exported pybind module

--- hydrogels/theory/models/_cxx/functions.cpp
+++ hydrogels/theory/models/_cxx/functions.cpp
@@ -1,25 +1,38 @@
 #include "functions.hpp"
 
-#define PI 3.14159
-
 double addNumbers (double a, double b) {
     double sum;
     sum = a+b;
     return sum;
 }
 
-double mass_from_R (double R, double rho) {
-    double mass = (4./3.) * PI * R * R * R * rho;
+double boltzmann (double beta, double U) {
+    double result = exp(beta * U * -1.0);
+    return result;
+}
+
+double massFromRadius (double R, double rho) {
+    double mass = (4./3.) * M_PI * R * R * R * rho;
     return mass;
 }
 
-double R_from_mass (double mass, double rho) {
-    double R = cbrt((0.75 / PI) * (1. / rho) * mass);
+double radiusFromMass (double mass, double rho) {
+    double R = cbrt((0.75 / M_PI) * (1.0 / rho) * mass);
     return R;
 }
 
-double k_R (double K_V, double c_0, double boltz) {
-    double result = K_V * c_0 * boltz;
+double radiusFromNumber (double N, double nV) {
+    double result = cbrt( ( 0.75 * M_PI ) * N * nV);
+    return result;
+}
+
+double rateFromPotentialEnergy (double K_V, double c_0, double U, double beta) {
+    double result = K_V * c_0 * boltzmann(beta, U);
+    return result;
+}
+
+double updateNumberFromRate (double N, double k, double dt) {
+    double result = N - k * dt;
     return result;
 }
 
@@ -30,4 +43,19 @@ double vectorNorm (std::vector<double> vect) {
         result += vect[i] * vect[i];
     result = sqrt(result);
     return result;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(functions, m) {
+    m.def("boltzmann", &boltzmann, R"pbdoc(Boltzmann Factor)pbdoc", py::arg("beta"), py::arg("U"));
+    m.def("radius_from_number", &radiusFromNumber, R"pbdoc(Radius from number of particles and number density)pbdoc", py::arg("N"), py::arg("nV"));
+    m.def("rate_from_potential_energy", &rateFromPotentialEnergy, R"pbdoc(Rate from Potential Energy at surface and bulk concentration)pbdoc", py::arg("KV"), py::arg("c0"), py::arg("U"), py::arg("beta"));
+    m.def("update_number_from_rate", &updateNumberFromRate, R"pbdoc(New number of particles using rate and timestep)pbdoc", py::arg("N"), py::arg("k"), py::arg("dt"));
+
+    #ifdef VERSION_INFO
+        m.attr("__version__") = VERSION_INFO;
+    #else
+        m.attr("__version__") = "dev";
+    #endif
 }
--- hydrogels/theory/models/_cxx/functions.hpp
+++ hydrogels/theory/models/_cxx/functions.hpp
@@ -2,9 +2,13 @@
 #define FUNCTION_HPP
 #include <bits/stdc++.h>
 #include <cmath>
-double addNumbers(double a, double b);
-double mass_from_R (double R, double rho);
-double R_from_mass (double mass, double rho);
-double k_R (double K_V, double c_0, double boltz);
+#include "pybind11/pybind11.h"
+double addNumbers (double a, double b);
+double massFromRadius (double R, double rho);
+double radiusFromMass (double mass, double rho);
+double boltzmann (double beta, double U);
+double rateFromPotentialEnergy (double K_V, double c_0, double U, double beta);
+double radiusFromNumber (double N, double nV);
+double updateNumberFromRate (double N, double k, double dt);
 double vectorNorm (std::vector<double> vect);
 #endif
