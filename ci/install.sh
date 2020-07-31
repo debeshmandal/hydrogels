87d828e89de589e14d39cc673bc4c5e525e810fd
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-31 13:25:48 +0100

    reorganised compiler if-else statements

--- ci/install.sh
+++ ci/install.sh
@@ -37,13 +37,12 @@ echo "create test-environment";
 conda env create -n test-environment -f ./ci/environment.yml;
 conda activate test-environment
 conda install pytest
-if [[ "$TRAVIS_OS_NAME" != "windows" ]]; then
-  if [[ "$TRAVIS_OS_NAME" == "osx"]]; then
+if  [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    conda install libpython m2w64-toolchain -c msys2
+if [[ "$TRAVIS_OS_NAME" == "osx"]]; then
     conda install clang_osx-64
-  elif [[ "$TRAVIS_OS_NAME" == "osx"]]; then
+else
     conda install gxx_linux-64
-elif  [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    conda install libpython m2w64-toolchain -c msys2 
 fi;
 conda list
 pip install . -vvv
