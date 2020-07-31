0a687fa067784c043326a84669ffd1cae9f92676
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-31 13:06:25 +0100

    added verbosity to pip install

--- ci/install.sh
+++ ci/install.sh
@@ -38,5 +38,5 @@ conda env create -n test-environment -f ./ci/environment.yml;
 conda activate test-environment
 conda install pytest
 conda list
-python -m pip install .
-python -m pip install cibuildwheel
+pip install . -vvv
+pip install cibuildwheel
