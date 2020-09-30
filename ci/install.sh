c351337d95484ad704dd5fc16c17d7dbb337015a
Debesh-XPS <debesh.mandal@hotmail.com>
2020-09-30 19:03:29 +0100

    using setup.py to install readdy

--- ci/install.sh
+++ ci/install.sh
@@ -4,16 +4,11 @@ python3 -m pip install numpy
 python3 -m pip install scipy
 python3 -m pip install pandas
 python3 -m pip install matplotlib
-python3 -m pip install .
+
 python3 -m pip install "pybind11[global]"
 python3 -m pip install h5py
 
 git clone https://github.com/readdy/readdy
-cd readdy
-mkdir build
-cd build
-cmake ..
-make -j8
-cd ..
-cd ..
 python3 -m pip install ./readdy
+
+python3 -m pip install .
