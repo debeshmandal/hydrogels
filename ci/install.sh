98ba3b02b61d0632e26cd2d4c480bf81d3dfcc31
Debesh-XPS <debesh.mandal@hotmail.com>
2020-09-30 18:38:25 +0100

    upgrading pip and using python3 -m pip install

--- ci/install.sh
+++ ci/install.sh
@@ -1,1 +1,7 @@
-pip install .
+python3 -m pip install --upgrade pip
+python3 -m pip install cibuildwheel==1.6.1
+python3 -m pip install numpy
+python3 -m pip install scipy
+python3 -m pip install pandas
+python3 -m pip install matplotlib
+python3 -m pip install .
