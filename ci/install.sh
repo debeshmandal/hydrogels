d171ffe82b02c25c042863f55c0beb1284288e19
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-01 18:44:22 +0100

    added all OSs again and added a get hdf5 sscript

--- ci/install.sh
+++ ci/install.sh
@@ -1,15 +1,46 @@
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install numpy
-python3 -m pip install scipy
-python3 -m pip install pandas
-python3 -m pip install matplotlib
+pip3 install --upgrade pip setuptools wheel
+pip3 install numpy
+pip3 install scipy
+pip3 install pandas
+pip3 install matplotlib
 
-python3 -m pip install "pybind11[global]"
-python3 -m pip install h5py
+pip3 install "pybind11[global]"
+pip3 install h5py
+
+#!/bin/bash
+
+set -e
+
+if [ -z ${HDF5_DIR+x} ]; then
+    echo "Using OS HDF5"
+else
+    echo "Using downloaded HDF5"
+    #python3 -m pip install requests
+    #python3 ci/get_hdf5.py
+    if [ -f $HDF5_DIR/lib/libhdf5.so ]; then
+	echo "using cached build"
+    else
+        pushd /tmp
+        #                             Remove trailing .*, to get e.g. '1.12' ↓
+        wget "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
+        tar -xzvf hdf5-$HDF5_VERSION.tar.gz
+        pushd hdf5-$HDF5_VERSION
+        chmod u+x autogen.sh
+        if [[ "${HDF5_VERSION%.*}" = "1.12" ]]; then
+          ./configure --prefix $HDF5_DIR
+        else
+          ./configure --prefix $HDF5_DIR
+        fi
+        make -j $(nproc)
+        make install
+        popd
+        popd
+    fi
+fi
 
 git clone https://github.com/readdy/readdy
-python3 -m pip install ./readdy
+pip3 install ./readdy
 
-python3 -m pip install .
+pip3 install .
 
-python3 -m pip install cibuildwheel==1.6.1
+pip3 install cibuildwheel==1.6.1
