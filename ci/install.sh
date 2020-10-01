17abac77fee96ed7171796cfd5cee477a1a9b48e
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-01 19:07:17 +0100

    trying to install hdf5

--- ci/install.sh
+++ ci/install.sh
@@ -21,14 +21,10 @@ else
     pushd /tmp
     #                             Remove trailing .*, to get e.g. '1.12' ↓
     wget "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
-    tar -xzvf hdf5-$HDF5_VERSION.tar.gz
+    tar -xzf hdf5-$HDF5_VERSION.tar.gz
     pushd hdf5-$HDF5_VERSION
     chmod u+x autogen.sh
-    if [[ "${HDF5_VERSION%.*}" = "1.12" ]]; then
-        ./configure --prefix $HDF5_DIR
-    else
-        ./configure --prefix $HDF5_DIR
-    fi
+    ./configure --prefix $HDF5_DIR
     make -j $(nproc)
     make install
     popd
