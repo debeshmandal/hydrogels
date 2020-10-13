d0bbc36ef1efff6899e9990ec5fd676ef01b175b
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-13 14:44:04 +0100

    trying to fix hdf5 issue

--- ci/install.sh
+++ ci/install.sh
@@ -28,9 +28,10 @@ pip3 install scipy
 pip3 install pandas
 pip3 install matplotlib
 
-pip3 install "pybind11"
+pip3 install pybind11
 pip3 install h5py
 
+export HDF5_DIR=/usr/local/HDF_Group/HDF5/1.12.0/share/cmake 
 git clone https://github.com/readdy/readdy
 pip3 install ./readdy
 
