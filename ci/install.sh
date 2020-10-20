8b30c0c83649af3602e568ae42e94358437b06e9
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-20 17:08:38 +0100

    added tqdm to the installs

--- ci/install.sh
+++ ci/install.sh
@@ -1,32 +1,9 @@
-echo $TRAVIS_OS_NAME
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    echo "LINUX"
-    wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.0/src/CMake-hdf5-1.12.0.tar.gz
-    tar xzf CMake-hdf5-1.12.0.tar.gz 
-    pushd CMake-hdf5-1.12.0
-    ./build-unix.sh
-    popd # leaving hdf5 directory
-elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    echo "OSX"
-    wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.0/src/CMake-hdf5-1.12.0.tar.gz
-    tar xzf CMake-hdf5-1.12.0.tar.gz 
-    pushd CMake-hdf5-1.12.0
-    ./build-unix.sh
-    popd # leaving hdf5 directory
-elif [[ $TRAVIS_OS_NAME == "windows" ]]; then
-    choco install zip
-    wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.0/src/CMake-hdf5-1.12.0.zip
-    tar.exe -x -f CMake-hdf5-1.12.0.zip
-    pushd CMake-hdf5-1.12.0
-    build-VS2017-64
-    popd # leaving hdf5 directory
-fi;
-
 pip3 install --upgrade pip setuptools wheel
 pip3 install numpy
 pip3 install scipy
 pip3 install pandas
 pip3 install matplotlib
+pip3 install tqdm
 
 pip3 install pybind11
 pip3 install h5py
