8e30cd2536c959b3e297e6343d3bf34ff0b03cce
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-23 18:36:35 +0100

    tidied up CI

--- ci/setup.sh
+++
@@ -1,7 +0,0 @@
-if [[ "$TRAVIS_OS_NAME" != "linux" ]]; then
-  echo "LINUX"
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  echo "OSX"
-elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-  echo "WINDOWS"
-fi;
