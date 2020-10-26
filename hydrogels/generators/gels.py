9a1b1b98a696cc6c45c15add16af40c8c65bdaf0
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-26 19:05:20 +0000

    initial implementation of oxdna reader - needs testing

--- hydrogels/generators/gels.py
+++ hydrogels/generators/gels.py
@@ -111,18 +111,6 @@ class PDMS(AbstractGel):
             direction = np.array([1., 0., 0.])
             mol = self.crosslinker(start_pos, direction)
             self._topologies.append(mol)
-    
-class Diacrylate(AbstractGel):
-    """
-    Diacrylate gels are made up of polymer chains where
-    the polymer chains where the terminal ends are able to
-    bond to each other.
-    
-    This means that all chains are identical and reactions
-    can occur between the terminal ends of each chain.
-    """
-    def __init__(self):
-        pass
 
 class LennardJonesGel(AbstractGel):
     """
@@ -315,3 +303,47 @@ class LennardJonesGel(AbstractGel):
     # add reaction that when an enzyme encounters a gel particle, it changes from bonded to unbonded
     # and its epsilon is set to zero and sigma set so that it is purely repulsive
 
+class OXDNAGel(AbstractGel):
+    """
+    Diacrylate gels are made up of polymer chains where
+    the polymer chains where the terminal ends are able to
+    bond to each other.
+    
+    This means that all chains are identical and reactions
+    can occur between the terminal ends of each chain.
+    """
+    def __init__(self, fname: str, **kwargs):
+        _box, _positions, _edges = self.read_oxDNA(fname)
+        self._box = np.array(_box)
+        self._positions = np.array(_positions)
+        self._edge_list = _edges
+        super().__init__(np.array(self._box), **kwargs)
+
+    def read_oxDNA(self):
+        positions = []
+        with open(oxdna_bonds, 'r') as f:
+            N = int(f.readline().split()[2])
+            edges = []
+            box = [float(i) for i in f.readline().split()[:3]]
+            
+            for i in range(N):
+                positions.append([float(i) for i in f.readline().split()])
+            print(pd.DataFrame(positions))
+            for i in range(N):
+                index, n_bonds = [int(i) for i in f.readline().split()]
+                edges.append([int(i) for i in f.readline().split()])
+                assert len(edges[index-1]) == n_bonds
+            print(pd.DataFrame(edges, dtype=int))           
+
+        return box, positions, edges
+
+    @property
+    def positions(self):
+        return self._positions
+
+    def generate_edges(self, *args, **kwargs):
+        edges = []
+        for i, indices in self._edge_list:
+            for j in indices:
+                edges.append([i, j])
+        return edges
