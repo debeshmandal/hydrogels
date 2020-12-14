fa4024dd7b3b45b5803423dc788928c4f930d3bd
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2020-12-14 22:31:26 +0000

    lammps runs but now needs a bit of tweaking

--- protocols/oxdna-generation/equilibrate/_equilibrate.py
+++ protocols/oxdna-generation/equilibrate/_equilibrate.py
@@ -1,12 +1,22 @@
+import subprocess
+
+from pathlib import Path
+
 from softnanotools.logger import Logger
 logger = Logger('EQUILIBRATE')
 
-def main(fname):
+def main(fname: str, lammps: str):
+    input_file = Path(__file__).resolve().replace('_equilibrate.py', 'lammps.main.in')
+    subprocess.check_output([
+        lammps,
+        input_file
+    ])
     return
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('fname')
+    parser.add_argument('-l', '--lammps', default='lmp_mpi')
     main(**vars(parser.parse_args()))
     
