0d0e0fdfd90065fb4b5d866d4a8ce9cacc26b979
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2020-12-30 00:02:18 +0000

    added test file

---
+++ protocols/gel-analysis/main.py
@@ -0,0 +1,10 @@
+from softnanotools.logger import Logger
+logger = Logger('MAIN')
+
+def main():
+    return
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    main()
---
+++ protocols/gel-analysis/model.py
@@ -0,0 +1,19 @@
+from softnanotools.logger import Logger
+logger = Logger('MODEL')
+
+def read_lammps_conf(fname):
+    data = {}
+    with open(fname, 'r') as f:
+        pass
+    return data
+
+class Model:
+    def __init__(self, particles, bonds):
+        self.particles = positions
+        self.bonds = edges
+
+    def rdf(self):
+        return
+
+    def gyration(self):
+        return
