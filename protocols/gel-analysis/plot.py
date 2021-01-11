0b3e8883de67a76561b1ea2b01d502105c39939e
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2021-01-11 16:31:52 +0000

    edits to running analysis simulation

--- protocols/gel-analysis/plot.py
+++ protocols/gel-analysis/plot.py
@@ -2,26 +2,58 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data.out', comment='#', skiprows=4, delim_whitespace=True, header=None)
+class Model:
+    def __init__(self, folder):
+        self._gyr = 'out.gyr'
+        self._rdf = 'out.rdf'
+        self._rho_N = 'out.rho.N'
+        self._rho_X = 'out.rho.X'
+    
+    @property
+    def json(self):
+        return
 
-fig, ax = plt.subplots()
+    @property
+    def rdf(self):
+        return
 
-for i, j in [(2, 'N'), (5, 'X')]:
-    ax.plot(data[1], data[i]/sum(data[i]), label=j)
+    @property
+    def gyration(self):
+        return
 
-ax.legend()
-ax.set_xlabel('r')
-ax.set_ylabel('g(r)')
+def plot_rdf(ax):
+    data = pd.read_csv(
+        'out.rdf',
+        delim_whitespace=True,
+        header=None,
+        skiprows=4,
+    )
+    delta = data[1][1] - data[1][0]
+    ax.bar(data[1] - delta/4, data[2], width=delta/2, label='N')
+    ax.bar(data[1] + delta/4, data[5], width=delta/2, label='X')
+    ax.legend(frameon=False)
+    ax.set_xlabel(r"$r$  $[\sigma]$")
+    ax.set_ylabel(r"$g(r)$")
+    return
 
-fig, ax = plt.subplots()
-data = pd.read_csv('gyr.out', comment='#', header=None, delim_whitespace=True)
-ax.plot(data[0], data[1])
-ax.set_xlabel('time')
-ax.set_ylabel('gyration')
+def plot_rho(ax):
+    data = pd.read_csv(
+        'out.rho.N',
+        delim_whitespace=True,
+        header=None,
+        skiprows=4,
+    )
+    ax.bar(data[1], data[3]/(4 * np.pi * data[1] ** 2 * (data[1][1]-data[1][0])), label='N')
+    ax.set_xlabel(r"$r$  $[\sigma]$")
+    ax.set_ylabel(r"$\rho(r)$")
+    return
 
-fig, ax = plt.subplots()
-ax.plot(data[0], data[2])
-ax.set_xlabel('time')
-ax.set_ylabel('mesh size')
+def main():
+    fig, ax = plt.subplots(2)
+    plot_rdf(ax[0])
+    plot_rho(ax[1])
+    plt.show()
 
-plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ protocols/gel-analysis/simulation/run.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+
+import subprocess
+import re
+
+import pandas as pd
+import numpy as np
+
+from softnanotools.logger import Logger
+logger = Logger('SIMULATION')
+
+def read_lammps_conf(fname):
+    data = {}
+    with open(fname, 'r') as f:
+
+        # skip first two lines
+        f.readline()
+        f.readline()
+
+        # read atom + bond types
+        logger.debug(f'Setting...')
+        for i in range(4):
+            line = f.readline().split()
+            value = line[0]
+            label = '_'.join(line[1:])
+            logger.debug(f'    {label}{" " * (10 - len(label))}\t-> {value}')
+            data[label] = int(value)
+
+        for i, line in enumerate(f.readlines()):
+            if re.findall('Atoms', line):
+                atoms_line = i+6
+                logger.debug(f'Line that atoms start on: {atoms_line}')
+
+            elif re.findall('Bonds', line):
+                bonds_line = i+6
+                logger.debug(f'Line that bonds start on: {bonds_line}')
+
+    data['particles'] = pd.read_csv(
+        fname, 
+        skiprows=atoms_line+2,
+        header=None,
+        delim_whitespace=True,
+        nrows=data['atoms']
+    ).rename(columns={
+        0: 'id',
+        1: 'mol',
+        2: 'type',
+        3: 'x',
+        4: 'y',
+        5: 'z',
+    })[['id', 'mol', 'type', 'x', 'y', 'z']].sort_values(
+        by='id'
+    ).reset_index(
+        drop=True
+    )
+
+    logger.debug(f'Particles:\n{data["particles"]}')
+    
+    data['edges'] = pd.read_csv(
+        fname, 
+        skiprows=bonds_line+1,
+        header=None,
+        delim_whitespace=True,
+        nrows=data['bonds']
+    ).rename(columns={
+        0: 'id',
+        1: 'type',
+        2: 'atom_1',
+        3: 'atom_2'
+    }).sort_values(by='id').reset_index(drop=True)
+
+    logger.debug(f'Edges:\n{data["edges"]}')
+
+    return data
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fname')
+    parser.add_argument('-T', '--temperature', default=0.05, type=float)
+    parser.add_argument('-l', '--lammps', default='lmp_mpi')
+    parser.add_argument('-p', '--prefix', default='mpirun -np 4')
+    parser.add_argument('-i', '--input-file', default='lammps.main.in')
+    args = parser.parse_args()
+
+    # cutoff
+
+    particles = read_lammps_conf(args.fname)['particles']
+    delta = [particles[i].max() - particles[i].min() for i in ['x', 'y', 'z']]
+    cutoff = max(delta)
+
+    # set variables
+
+    with open(args.input_file, 'r') as f:
+        lines = f.readlines()
+
+    for i, line in enumerate(lines):
+        if re.findall('variable fname string', line):
+            logger.debug(f'Setting fname in line {i} to {args.fname}')
+            lines[i] = f'variable fname string {args.fname}\n'
+        if re.findall('variable T equal', line):
+            logger.debug(f'Setting T in line {i} to {args.temperature}')
+            lines[i] = f'variable T equal {args.temperature}\n'
+        if re.findall('variable global_cutoff equal', line):
+            logger.debug(f'Setting global_cutoff in line {i} to {cutoff}')
+            lines[i] = f'variable global_cutoff equal {cutoff}\n'
+
+
+    with open(args.input_file, 'w') as f:
+        f.write(''.join(lines))
+
+    # run simulation
+
+    if args.prefix:
+        subprocess.check_output([
+            *args.prefix.split(),
+            args.lammps,
+            '-i',
+            args.input_file
+        ])
+
+    else:
+        subprocess.check_output(
+            args.lammps,
+            '-i',
+            args.input_file
+        )
+
