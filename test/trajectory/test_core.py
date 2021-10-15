177ccfe501ed573c459f89f62fb0146fa843942c
Debesh Mandal <debesh.mandal@hotmail.com>
2021-10-15 18:15:06 +0100

    writing to lammps conf seems to work for topology and particle collections

--- test/trajectory/test_core.py
+++ test/trajectory/test_core.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
-from hydrogels.trajectory.core import ParticleFrame, ParticleTrajectory
+from hydrogels.trajectory.core import (
+    ParticleFrame,
+    ParticleTrajectory,
+    TopologyFrame,
+    TopologyTrajectory
+)
 
 FOLDER = Path(__file__).parent
 H5 = FOLDER / '_test.h5'
@@ -22,6 +27,18 @@ def test_ParticleTrajectory():
     traj.box
     return
 
+def test_TopologyFrame():
+    traj = TopologyTrajectory(H5)
+    frame = traj.frames[0]
+    frame.dataframe
+
+def test_TopologyTrajectory():
+    traj = TopologyTrajectory(H5)
+    traj.time
+    traj.frames
+
 if __name__ == '__main__':
     test_ParticleFrame()
     test_ParticleTrajectory()
+    test_TopologyFrame()
+    test_TopologyTrajectory()
--- test/trajectory/test_lammps.py
+++ test/trajectory/test_lammps.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from hydrogels.trajectory.core import ParticleTrajectory
+from hydrogels.trajectory.core import ParticleTrajectory, TopologyTrajectory
 
 FOLDER = Path(__file__).parent
 H5 = FOLDER / '_test.h5'
@@ -10,10 +10,22 @@ def test_write_LAMMPS_dump():
     frame = traj.frames[0]
     frame.array
     frame.dataframe
-    output = FOLDER / 'lammps.test.conf'
+    output = FOLDER / 'lammps.test.dump'
     frame.to_LAMMPS_dump(output)
     output.unlink()
     return
 
+def test_write_LAMMPS_configuration():
+    traj = ParticleTrajectory(H5)
+    particles = traj.frames[0]
+    traj = TopologyTrajectory(H5)
+    topology = traj.frames[0]
+
+    output = FOLDER / 'lammps.test.conf'
+    particles.to_LAMMPS_configuration(output, topology)
+    #output.unlink()
+    return
+
 if __name__ == '__main__':
     test_write_LAMMPS_dump()
+    test_write_LAMMPS_configuration()
