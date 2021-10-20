e3b4f5ca4ea3b3e9209ab0b2e07a483b588e999a
Debesh Mandal <debesh.mandal@hotmail.com>
2021-10-21 00:36:22 +0100

    again trying with the hdf5 versioning

--- test/trajectory/test_core.py
+++ test/trajectory/test_core.py
@@ -9,80 +9,77 @@ from hydrogels.trajectory.core import (
 
 import h5py
 
+from softnanotools.logger import Logger
+logger = Logger(__name__)
+
 FOLDER = Path(__file__).parent
 H5 = FOLDER / '_test.h5'
 
 def test_ParticleFrame():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-
-    traj = ParticleTrajectory(H5)
-    frame = traj.frames[0]
-    frame.array
-    frame.dataframe
-    output = FOLDER / 'lammps.test.conf'
-    frame.to_LAMMPS_dump(output)
-    output.unlink()
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = ParticleTrajectory(H5)
+        frame = traj.frames[0]
+        frame.array
+        frame.dataframe
+        output = FOLDER / 'lammps.test.conf'
+        frame.to_LAMMPS_dump(output)
+        output.unlink()
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
     return
 
 def test_ParticleTrajectory():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    traj = ParticleTrajectory(H5)
-    traj.time
-    traj.frames
-    traj.box
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = ParticleTrajectory(H5)
+        traj.time
+        traj.frames
+        traj.box
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
     return
 
 def test_TopologyFrame():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    traj = TopologyTrajectory(H5)
-    frame = traj.frames[0]
-    frame.dataframe
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = TopologyTrajectory(H5)
+        frame = traj.frames[0]
+        frame.dataframe
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
 
 def test_TopologyTrajectory():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    traj = TopologyTrajectory(H5)
-    traj.time
-    traj.frames
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = TopologyTrajectory(H5)
+        traj.time
+        traj.frames
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
 
 def test_LAMMPS_output():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    topology = TopologyTrajectory(H5)
-    particles = ParticleTrajectory(H5)
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        topology = TopologyTrajectory(H5)
+        particles = ParticleTrajectory(H5)
 
-    dump = FOLDER / 'dump'
-    particles.to_LAMMPS_dump(dump)
-    for target in FOLDER.glob('dump.*'):
-        target.unlink()
+        dump = FOLDER / 'dump'
+        particles.to_LAMMPS_dump(dump)
+        for target in FOLDER.glob('dump.*'):
+            target.unlink()
 
-    conf = FOLDER / 'conf'
-    particles.to_LAMMPS_configuration(conf, topology)
-    for target in FOLDER.glob('conf.*'):
-        target.unlink()
+        conf = FOLDER / 'conf'
+        particles.to_LAMMPS_configuration(conf, topology)
+        for target in FOLDER.glob('conf.*'):
+            target.unlink()
 
-    topology.to_LAMMPS_configuration(conf, particles)
-    for target in FOLDER.glob('conf.*'):
-        target.unlink()
+        topology.to_LAMMPS_configuration(conf, particles)
+        for target in FOLDER.glob('conf.*'):
+            target.unlink()
 
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
     return
 
 if __name__ == '__main__':
--- test/trajectory/test_lammps.py
+++ test/trajectory/test_lammps.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import h5py
+from softnanotools.logger import Logger
+logger = Logger(__name__)
 
 from hydrogels.trajectory.core import ParticleTrajectory, TopologyTrajectory
 
@@ -7,34 +9,32 @@ FOLDER = Path(__file__).parent
 H5 = FOLDER / '_test.h5'
 
 def test_write_LAMMPS_dump():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    traj = ParticleTrajectory(H5)
-    frame = traj.frames[0]
-    frame.array
-    frame.dataframe
-    output = FOLDER / 'lammps.test.dump'
-    frame.to_LAMMPS_dump(output)
-    output.unlink()
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = ParticleTrajectory(H5)
+        frame = traj.frames[0]
+        frame.array
+        frame.dataframe
+        output = FOLDER / 'lammps.test.dump'
+        frame.to_LAMMPS_dump(output)
+        output.unlink()
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
     return
 
 def test_write_LAMMPS_configuration():
-    try:
-        if h5py.h5.HDF5_VERSION_COMPILED_AGAINST[1] < 10:
-            return
-    except:
-        return
-    traj = ParticleTrajectory(H5)
-    particles = traj.frames[0]
-    traj = TopologyTrajectory(H5)
-    topology = traj.frames[0]
+    MINOR_VERSION = h5py.version.hdf5_version_tuple[1]
+    if MINOR_VERSION > 9:
+        traj = ParticleTrajectory(H5)
+        particles = traj.frames[0]
+        traj = TopologyTrajectory(H5)
+        topology = traj.frames[0]
 
-    output = FOLDER / 'lammps.test.conf'
-    particles.to_LAMMPS_configuration(output, topology)
-    output.unlink()
+        output = FOLDER / 'lammps.test.conf'
+        particles.to_LAMMPS_configuration(output, topology)
+        output.unlink()
+    else:
+        logger.warning(f'HDF5 Version is {h5py.version.hdf5_version}')
     return
 
 if __name__ == '__main__':
