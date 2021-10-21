a5012189c7232ef19433235197e560da739ffbf8
Debesh Mandal <debesh.mandal@hotmail.com>
2021-10-21 11:09:16 +0100

    added test for reaction

--- test/generators/gels/test_core.py
+++ test/generators/gels/test_core.py
@@ -13,7 +13,7 @@ def generate_gel():
             [1., 1., 0.],
             [0., 1., 0.],
             [0., 0., 0.],
-        ]) 
+        ])
     )
     gel.edges = [
         (0, 1),
@@ -24,14 +24,24 @@ def generate_gel():
         (2, 3),
     ]
     gel.configure_bonds('harmonic', force_constant=1.0, length=1.0)
+
     return gel
 
 def test_Gel():
     gel = generate_gel()
     system = System([10., 10., 10.])
     system.insert_topology(gel, diffusion_constant=1.0)
+    system.add_species('enzyme', 1.0)
+    system.add_species('released', 1.0)
+    gel.register_decay(system, reaction_type='polymer')
+    gel.register_degradation(system, rate=10.)
     simulation = system.initialise_simulation()
-    simulation.run(10, 0.1)
+    simulation.add_particles('enzyme', np.array([
+        [2.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0],
+        [0.0, 0.0, 2.0],
+    ]))
+    simulation.run(10000, 0.001)
     return
 
 if __name__ == '__main__':
